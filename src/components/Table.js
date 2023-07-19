79f968f72fca8a0165dbfce20875cc0d9d422cbb
RP <rp@RPdeMacBook-Pro-2.local>
2023-07-19 11:45:48 -0500

    finished GA6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,25 @@
+import './styling.css';
+
 const Table = ({ sat }) => {
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((date, id) => (
+            <tr key = {id}>
+              <td>{date.name}</td> 
+              <td>{date.typeOfSatellite}</td> 
+              <td>{date.launchDate}</td> 
+              <td>{date.operational? "Active" : "Inactive"}</td> 
+              </tr>
+          ))}
         </tbody>
       </table>
   );
